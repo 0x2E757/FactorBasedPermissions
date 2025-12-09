e5dd46ae5d560259ff45eafa52475724acc834df
Eric Kevrel <0x2E757@gmail.com>
2025-12-09 20:58:09 +0200

    Initial TypeScript implementation

---
+++ TypeScript/src/index.ts
@@ -0,0 +1,112 @@
+const SATISFIED_FACTORS_PREFIX = "!";
+const PERMISSIONS_PREFIX = "#";
+const ITEMS_DELIM = ",";
+const GROUP_DELIM = "&";
+const REQUIRED_FACTORS_DELIM = "+";
+
+const groupsRegex = buildGroupsRegex();
+
+function buildGroupsRegex() {
+  const groupPrefixes = SATISFIED_FACTORS_PREFIX + PERMISSIONS_PREFIX;
+  const satisfiedFactorsGroup = `(?:${SATISFIED_FACTORS_PREFIX}([^${groupPrefixes}]*))?`;
+  const permissionGroupsGroup = `(?:${PERMISSIONS_PREFIX}([^${groupPrefixes}]*))?`;
+  return new RegExp(satisfiedFactorsGroup + permissionGroupsGroup);
+}
+
+function parseNumeric<T extends number>(value: string) {
+  return parseInt(value, 32) as T;
+}
+
+export class FactorBasedPermissions<TFactor extends number, TPermission extends number> {
+  private _factorsLookup = new Map<TFactor, boolean>();
+  private _permissionsLookup = new Map<TPermission, TFactor[]>();
+  private _permissionCheckLookup = new Map<TPermission, boolean | null>();
+
+  public constructor(serialized?: string | null | undefined) {
+    if (typeof serialized === "string") {
+      const matches = groupsRegex.exec(serialized);
+      this._parseFactors(matches?.[1] ?? null);
+      this._parsePermissions(matches?.[2] ?? null);
+    }
+  }
+
+  private _parseFactors(satisfiedFactorsGroup: string | null) {
+    if (!satisfiedFactorsGroup) {
+      return;
+    }
+
+    for (const factor of satisfiedFactorsGroup.split(ITEMS_DELIM)) {
+      const factorNumeric = parseNumeric<TFactor>(factor);
+      this._factorsLookup.set(factorNumeric, true);
+    }
+  }
+
+  private _parsePermissions(permissionGroupsGroup: string | null) {
+    if (!permissionGroupsGroup) {
+      return;
+    }
+
+    for (const permissionGroup of permissionGroupsGroup.split(GROUP_DELIM)) {
+      const [permissionsRaw, requiredFactorsRaw] = permissionGroup.split(REQUIRED_FACTORS_DELIM);
+      const permissions = permissionsRaw?.split(ITEMS_DELIM)?.map(parseNumeric<TPermission>) ?? [];
+      const requiredFactors = requiredFactorsRaw?.split(ITEMS_DELIM)?.map(parseNumeric<TFactor>) ?? [];
+
+      for (const permission of permissions) {
+        this._permissionsLookup.set(permission, requiredFactors);
+      }
+
+      for (const requiredFactor of requiredFactors) {
+        if (!this._factorsLookup.has(requiredFactor)) {
+          this._factorsLookup.set(requiredFactor, false);
+        }
+      }
+    }
+  }
+
+  public checkPermission(permission: TPermission) {
+    const cachedResult = this._permissionCheckLookup.get(permission);
+
+    if (cachedResult !== undefined) {
+      return cachedResult;
+    }
+
+    const requiredFactors = this._permissionsLookup.get(permission);
+
+    if (!requiredFactors) {
+      this._permissionCheckLookup.set(permission, null);
+      return null;
+    }
+
+    for (const requiredFactor of requiredFactors) {
+      if (!this._factorsLookup.get(requiredFactor)) {
+        this._permissionCheckLookup.set(permission, false);
+        return false;
+      }
+    }
+
+    this._permissionCheckLookup.set(permission, true);
+    return true;
+  }
+
+  public getSatisfiedFactors(permission: TPermission) {
+    const cachedResult = this._permissionCheckLookup.get(permission);
+    const requiredFactors = this._permissionsLookup.get(permission) ?? [];
+
+    if (cachedResult === true) {
+      return requiredFactors;
+    }
+
+    return requiredFactors.filter((factor) => this._factorsLookup.get(factor));
+  }
+
+  public getMissingFactors(permission: TPermission) {
+    const cachedResult = this._permissionCheckLookup.get(permission);
+
+    if (cachedResult === true) {
+      return [];
+    }
+
+    const requiredFactors = this._permissionsLookup.get(permission) ?? [];
+    return requiredFactors.filter((factor) => !this._factorsLookup.get(factor));
+  }
+}
